d2072d183ea66cfb8f29e6dde006e0b9ed7909b2
Bernard Tison <bernard.tison@gmail.com>
2020-08-13 11:05:54 +0200

    Added MicroProfile FaultTolerance annotations

--- src/main/java/com/redhat/emergency/response/incident/finder/MissionService.java
+++ src/main/java/com/redhat/emergency/response/incident/finder/MissionService.java
@@ -4,6 +4,11 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
 import io.vertx.core.json.JsonObject;
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.Retry;
+import org.eclipse.microprofile.faulttolerance.Timeout;
+import org.eclipse.microprofile.faulttolerance.exceptions.TimeoutException;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 @ApplicationScoped
@@ -13,8 +18,16 @@ public class MissionService {
     @RestClient
     MissionServiceClient client;
 
+    @Retry(maxRetries = 2, maxDuration = 2500, abortOn = {TimeoutException.class})
+    @Timeout(500)
+    @CircuitBreaker(successThreshold = 5, requestVolumeThreshold = 4, failureRatio=0.75, delay = 5000)
+    @Fallback(fallbackMethod= "fallback")
     public JsonObject missionByIncidentId(String incidentId) {
         return client.missionByIncident(incidentId);
+    }
+
+    public JsonObject fallback(String incidentId) {
+        return null;
     }
 
 }
